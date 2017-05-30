493d9caafea23f0e9b1a520ab7a291202f698f42
glonglethorp\Sam <samhubler@gmail.com>
2017-05-30 17:02:23 -0400

    Sec 4.127 Added LineTraceSingleByChannel to TankPlayerController to determine location of crosshair overlap

--- TankBattle/Source/TankBattle/TankPlayerController.h
+++ TankBattle/Source/TankBattle/TankPlayerController.h
@@ -29,10 +29,15 @@ private:
 	UPROPERTY(EditAnywhere)
 	float CrossHairYLocation = 0.33333f;
 
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000.0f;
+
 	// Move the barrel of the tank where the crosshairs intersect with the world
 	void AimTowardsCrosshair();
 	
 	bool GetSightRayHitLocation(FVector &OutHitLocation) const;
 
 	bool GetLookDirection(FVector2D ScreenLocation, FVector &LookDirection) const;
+
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector &HitLocation) const;
 };
